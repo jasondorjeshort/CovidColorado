f7e2b19cc12707fcb47e327e9dce431a6efd585b
Jason Dorje Short <jdorje@gmail.com>
2020-11-24 12:51:54 -0700

    Add GPL tags.

--- src/main/java/covid/Date.java
+++ src/main/java/covid/Date.java
@@ -3,8 +3,26 @@ package covid;
 import org.jfree.data.time.Day;
 
 /**
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * -----
+ * 
  * Trivial reinvention of the wheel to turn a date in 2020 into the number of
  * days since the start of 2020, and back.
+ * 
+ * @author jdorje@gmail.com
  */
 public class Date {
 
