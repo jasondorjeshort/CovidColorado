3fe8992d72c19ab75e93decfec860b9e862fe247
Jason Dorje Short <jdorje@gmail.com>
2020-11-27 10:22:59 -0700

    Beginning code for a web server?

--- src/main/java/library/MyExecutor.java
+++ src/main/java/library/MyExecutor.java
@@ -4,6 +4,8 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,15 +19,16 @@ import java.util.concurrent.TimeUnit;
  * I might refine this more in future, perhaps even creating my own executor
  * service that can create threads based on the CPU usage.
  * 
- * @author jdorj
- *
+ * @author jdorje@gmail.com
  */
 public class MyExecutor {
 
 	private static final int processors = Runtime.getRuntime().availableProcessors();
 	private static final int threads = Math.max(processors / 2, 1);
 
+	private static final ExecutorService webPool = Executors.newFixedThreadPool(threads);
 	private static final ExecutorService codePool = Executors.newFixedThreadPool(threads);
+	private static final ScheduledExecutorService schedule = Executors.newSingleThreadScheduledExecutor();
 
 	static Runnable catchWrapper(Runnable command) {
 		return new Runnable() {
@@ -54,6 +57,10 @@ public class MyExecutor {
 		};
 	}
 
+	public static void executeWeb(Runnable command) {
+		webPool.execute(catchWrapper(command));
+	}
+
 	public static void executeCode(Runnable command) {
 		codePool.execute(catchWrapper(command));
 	}
@@ -62,22 +69,39 @@ public class MyExecutor {
 		return codePool.submit(catchWrapper(task));
 	}
 
+	public static <T> Future<T> submitWeb(Callable<T> task) {
+		return webPool.submit(catchWrapper(task));
+	}
+
+	public static void scheduleWeb(Runnable command, long delay, TimeUnit unit) {
+		schedule.schedule(catchWrapper(command), delay, unit);
+	}
+
+	public static ScheduledFuture<?> scheduleWebWithFixedDelay(Runnable command, long initialDelay, long delay,
+			TimeUnit unit) {
+		return schedule.scheduleWithFixedDelay(() -> webPool.execute(catchWrapper(command)), initialDelay, delay, unit);
+	}
+
 	public static void shutdown() {
+		webPool.shutdown();
 		codePool.shutdown();
+		schedule.shutdown();
 	}
 
 	public static boolean awaitTermination(long timeout, TimeUnit unit) {
 		shutdown();
 		try {
-			if (codePool.awaitTermination(timeout, unit)) {
+			if (codePool.awaitTermination(timeout, unit) && webPool.awaitTermination(timeout, unit)) {
+				System.out.println("Successfully waited out termination.");
 				return true;
 			}
+			webPool.shutdownNow();
 			codePool.shutdownNow();
-			if (codePool.awaitTermination(timeout, unit)) {
-				System.out.println("Forced thread pool termination.");
+			if (codePool.awaitTermination(timeout, unit) && webPool.awaitTermination(timeout, unit)) {
+				System.out.println("Successfully forced termination.");
 				return true;
 			}
-			System.out.println("Forced thread pool termination.");
+			System.out.println("Pool forced down.");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 			return false;
