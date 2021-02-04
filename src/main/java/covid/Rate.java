8e4d72995a011c404ee9ff5aa707653df214e788
Jason Dorje Short <jdorje@gmail.com>
2021-02-04 16:36:23 -0700

    Add comments and copyright.

--- src/main/java/covid/Rate.java
+++ src/main/java/covid/Rate.java
@@ -1,17 +1,38 @@
 package covid;
 
 import java.awt.Color;
+
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * @author jdorje@gmail.com
+ */
 public enum Rate {
 
+	/**
+	 * A rate is a ratio between two numbers, expressed as a percentage.
+	 * 
+	 * That's basically it. We mostly talk about positivity and CFR, but any
+	 * pair of numbers (NumbersType) could be divided to get a proportion.
+	 */
 	POSITIVITY(NumbersType.CASES, NumbersType.TESTS, Color.BLUE, 25, false),
 	CFR(NumbersType.DEATHS, NumbersType.CASES, Color.BLACK, 10, true),
 	CHR(NumbersType.DEATHS, NumbersType.HOSPITALIZATIONS, Color.RED, 40, true),
-	HFR(NumbersType.CASES, NumbersType.TESTS, Color.ORANGE, 100, true),
-
-	;
+	HFR(NumbersType.CASES, NumbersType.TESTS, Color.ORANGE, 100, true),;
 
 	public final NumbersType numerator, denominator;
 	public final String lowerName = name().toLowerCase();
