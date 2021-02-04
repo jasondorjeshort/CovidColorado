abacf432597330b9820cbb8e6299b20a35ce3f72
Jason Dorje Short <jdorje@gmail.com>
2021-02-03 21:11:44 -0700

    Change incomplete charts to use a DeviationRenderer with a collection of YIntervalSeries.  I guess this is the correct way, huh?

--- src/main/java/charts/ChartIncompletes.java
+++ src/main/java/charts/ChartIncompletes.java
@@ -1,5 +1,7 @@
 package charts;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.io.File;
 import java.util.Set;
 
@@ -9,12 +11,20 @@ import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.LogarithmicAxis;
 import org.jfree.chart.plot.ValueMarker;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.DeviationRenderer;
 import org.jfree.chart.renderer.xy.XYAreaRenderer;
+import org.jfree.chart.ui.RectangleInsets;
 import org.jfree.data.time.Day;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.TimeTableXYDataset;
+import org.jfree.data.xy.IntervalXYDataset;
+import org.jfree.data.xy.XYIntervalSeries;
+import org.jfree.data.xy.XYIntervalSeriesCollection;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.xy.YIntervalSeries;
+import org.jfree.data.xy.YIntervalSeriesCollection;
 
 import com.madgag.gif.fmsware.AnimatedGifEncoder;
 
@@ -24,6 +34,7 @@ import covid.Event;
 import covid.IncompleteNumbers;
 import covid.NumbersTiming;
 import covid.NumbersType;
+import covid.Smoothing;
 
 /**
  * This program is free software: you can redistribute it and/or modify it under
@@ -50,9 +61,12 @@ public class ChartIncompletes {
 
 	public Chart buildChart(String folder, String fileName, int dayOfData, Set<NumbersType> types, NumbersTiming timing,
 			boolean logarithmic) {
-		XYSeriesCollection collection = new XYSeriesCollection();
-		String verticalAxis = null;
+		YIntervalSeriesCollection collection = new YIntervalSeriesCollection();
 		StringBuilder title = new StringBuilder();
+
+		// https://www.jfree.org/forum/viewtopic.php?t=20396
+		DeviationRenderer renderer = new DeviationRenderer(true, false);
+		int seriesCount = 0;
 
 		boolean multi = (types.size() > 1);
 		int firstDayOfChart = Integer.MAX_VALUE;
@@ -91,29 +105,27 @@ public class ChartIncompletes {
 			if (!numbers.hasData()) {
 				continue;
 			}
-			XYSeries series = new XYSeries(type.capName);
+			Smoothing smoothing = type.smoothing; // Smoothing.NONE;
+			YIntervalSeries series = new YIntervalSeries(type.capName + " (" + smoothing.getDescription() + ")");
 
 			firstDayOfChart = Math.min(firstDayOfChart, numbers.getFirstDayOfType());
 			for (int d = numbers.getFirstDayOfType(); d <= dayOfData; d++) {
 				long time = CalendarUtils.dayToTime(d);
 
-				double upperBound = numbers.getNumbers(dayOfData, d, IncompleteNumbers.Form.UPPER, type.smoothing);
-				if (!logarithmic || upperBound > 0) {
-					series.add(time, upperBound);
-				}
-				double lowerBound = numbers.getNumbers(dayOfData, d, IncompleteNumbers.Form.LOWER, type.smoothing);
-				if (!logarithmic || lowerBound > 0) {
-					series.add(time + 1, lowerBound);
+				double upperBound = numbers.getNumbers(dayOfData, d, IncompleteNumbers.Form.UPPER, smoothing);
+				double lowerBound = numbers.getNumbers(dayOfData, d, IncompleteNumbers.Form.LOWER, smoothing);
+				double proj = numbers.getNumbers(dayOfData, d, IncompleteNumbers.Form.PROJECTED, smoothing);
+				if (!logarithmic || (lowerBound > 0 && proj > 0 && upperBound > 0)) {
+					series.add(time, proj, lowerBound, upperBound);
 				}
 			}
 
 			collection.addSeries(series);
 
-			if (verticalAxis == null) {
-				verticalAxis = type.capName;
-			} else {
-				verticalAxis = verticalAxis + " / " + type.capName;
-			}
+			renderer.setSeriesStroke(seriesCount, new BasicStroke(1.5f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+			renderer.setSeriesPaint(seriesCount, type.color);
+			renderer.setSeriesFillPaint(seriesCount, type.color.darker());
+			seriesCount++;
 		}
 
 		if (firstDayOfChart >= stats.getLastDay()) {
@@ -123,10 +135,12 @@ public class ChartIncompletes {
 
 		// dataset.addSeries("Cases", series);
 
+		String verticalAxis = NumbersType.name(types, " / ");
 		JFreeChart chart = ChartFactory.createTimeSeriesChart(title.toString(), "Date", verticalAxis, collection);
 
 		XYPlot plot = chart.getXYPlot();
-		//plot.setRenderer(new XYAreaRenderer());
+		plot.setRenderer(renderer);
+
 		if (logarithmic) {
 			LogarithmicAxis yAxis = new LogarithmicAxis(verticalAxis);
 			yAxis.setLowerBound(1);
