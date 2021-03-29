380d5ea7436568f00e7f41bb2564dfac3ee6b66d
Jason Dorje Short <jdorje@gmail.com>
2021-03-29 16:35:36 -0600

    Best make it 200 day range so that a 2.5%->97.5% discrete interval is actually discrete.

--- src/main/java/charts/AbstractChart.java
+++ src/main/java/charts/AbstractChart.java
@@ -27,7 +27,7 @@ import library.ASync;
 public abstract class AbstractChart {
 
 	public static final int DELAY = 30;
-	public static final int INTERVAL = 180;
+	public static final int INTERVAL = 200; // best work with the 95%
 	public static final double confidence = 95;
 	public static final double sideRange = (100 - confidence) / 2;
 
