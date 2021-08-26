f9b1991f09fdaf75f4a8c90398728c9a49d28fa3
Jason Dorje Short <jdorje@gmail.com>
2021-08-26 15:50:51 -0600

    Let's up delay to 8 weeks.

--- src/main/java/charts/AbstractChart.java
+++ src/main/java/charts/AbstractChart.java
@@ -26,7 +26,7 @@ import library.ASync;
  */
 public abstract class AbstractChart {
 
-	public static final int DELAY = 28; // multiple of 7
+	public static final int DELAY = 56; // multiple of 7
 	public static final int INTERVAL = 160; // best work with the 95%
 	public static final double confidence = 95;
 	public static final double bottomRange = (100 - confidence) / 2;
