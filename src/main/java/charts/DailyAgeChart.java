31f9574c502df54b80e74983b3b9b35b95e7a01d
Jason Dorje Short <jdorje@gmail.com>
2022-04-22 20:42:15 -0600

    Much better work for dailyage to be weekly!

--- src/main/java/charts/DailyAgeChart.java
+++ src/main/java/charts/DailyAgeChart.java
@@ -1,11 +1,13 @@
 package charts;
 
+import java.awt.BasicStroke;
 import java.util.Set;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.DeviationRenderer;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 
@@ -50,40 +52,53 @@ public class DailyAgeChart extends AbstractChart {
 
 	@Override
 	public boolean publish(int dayOfData) {
+		if (dayOfData != stats.getLastDay()) {
+			return false;
+		}
 		if (timing == NumbersTiming.ONSET && types.size() == 3) {
 			return true;
 		}
 		return false;
 	}
 
+	public static final int AGE_INTERVAL = 7;
+
 	@Override
 	public JFreeChart buildChart(int dayOfData) {
 
 		TimeSeriesCollection collection = new TimeSeriesCollection();
+		DeviationRenderer renderer = new DeviationRenderer(true, false);
+		int seriesCount = 0;
 
 		for (NumbersType type : types) {
 			IncompleteNumbers numbers = stats.getNumbers(type, timing);
-			TimeSeries series = new TimeSeries(type.capName);
 
-			if (!numbers.dayHasData(dayOfData)) {
+			if (!numbers.dayHasData(dayOfData) || !numbers.dayHasData(dayOfData - AGE_INTERVAL)) {
 				continue;
 			}
 
+			double total = 0;
 			for (int dayOfType = numbers.getFirstDayOfType(); dayOfType <= dayOfData; dayOfType++) {
-				Double number = numbers.getNewNumbers(dayOfData, dayOfType, 7);
-
-				if (number == null) {
-					continue;
-				}
-
-				series.add(CalendarUtils.dayToDay(dayOfType), number);
+				total += numbers.getNewNumbers(dayOfData, dayOfType, 7);
+			}
+			if (total <= 0) {
+				continue;
+			}
+			TimeSeries series = new TimeSeries(String.format("%s (%,d)", type.capName, Math.round(total)));
+			for (int dayOfType = numbers.getFirstDayOfType(); dayOfType <= dayOfData; dayOfType++) {
+				double number = numbers.getNewNumbers(dayOfData, dayOfType, AGE_INTERVAL);
+				series.add(CalendarUtils.dayToDay(dayOfType), 100.0 * number / total);
 			}
 
 			collection.addSeries(series);
+			renderer.setSeriesStroke(seriesCount, new BasicStroke(1.5f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+			renderer.setSeriesPaint(seriesCount, type.color);
+			renderer.setSeriesFillPaint(seriesCount, type.color.darker());
+			seriesCount++;
 		}
 
 		StringBuilder title = new StringBuilder();
-		title.append("Date of " + timing.lowerName + " of newly released \n");
+		title.append("Date of " + timing.lowerName + " of newly released ");
 		if (types.size() > 1) {
 			title.append("numbers");
 		} else {
@@ -91,11 +106,14 @@ public class DailyAgeChart extends AbstractChart {
 				title.append(type.lowerName);
 			}
 		}
-		title.append(" for ");
-		title.append(CalendarUtils.dayToDate(dayOfData));
+		title.append(" (%) \n");
+		title.append(String.format("from %s to %s", CalendarUtils.dayToDate(dayOfData - AGE_INTERVAL),
+				CalendarUtils.dayToDate(dayOfData)));
+
 		JFreeChart chart = ChartFactory.createTimeSeriesChart(title.toString(), "Date", "Count", collection);
 
 		XYPlot plot = chart.getXYPlot();
+		plot.setRenderer(renderer);
 
 		DateAxis xAxis = new DateAxis("Date");
 		int last = stats.getLastDay();
@@ -109,7 +127,7 @@ public class DailyAgeChart extends AbstractChart {
 
 	@Override
 	public String getName() {
-		return NumbersType.name(types, "-") + "-" + timing.lowerName;
+		return "daily-age-" + NumbersType.name(types, "-") + "-" + timing.lowerName;
 	}
 
 	@Override
